404244c5c855a3fa7b4c9857af43ed0a171288ca
William Song <30965609+Freakwill@users.noreply.github.com>
2019-01-07 12:22:39 +0800

    Add files via upload

--- Prime.hs
+++ Prime.hs
@@ -1,38 +1,36 @@
 module Prime
 where 
 
+import Data.List
+
 set :: [a] -> Int -> a -> [a]
 set [] n b = []
 set (a:as) 0 b = b:as
 set (a:as) n b = a : (set as (n - 1) b)
-
-slice :: Int -> Int -> Int -> [Int]
-slice start end step = if (end < start) then [] else (start : (slice (start + step) end step))
 
 
 setfalse :: [Int] -> [Bool] -> [Bool]
 setfalse [] a = a
 setfalse (i:is) a = set (setfalse is a) i False
 
-isEven ::Int -> Bool 
-isEven x = x `mod` 2 == 0
 
-isPrime :: Int -> [Bool]
-isPrime 1 = [True, True]
-isPrime 2 = [False, False, True]
-isPrime n = foldl (\l -> \p-> (isPrime_ l p n)) f (slice 3 (floor (sqrt n)) 2)
-    where f = [if (isEven x) then False else True | x <- [0..n]]
+-- isPrime :: Int -> [Bool]
+-- isPrime 1 = [True, True]
+-- isPrime 2 = [False, False, True]
+-- isPrime n = let f = [if (x `mod` 2 == 0) then False else True | x <- [0..n]] in
+--  foldl removeComp f (slice 3 (floor (sqrt n)) 2) 
 
-isPrime_ :: [Bool] -> Int -> Int -> [Bool]
-isPrime_ l p n = if l!!p then (setfalse (slice (p^2) n (2*p)) l) else l
+removeComp :: [Bool] -> Int -> [Bool]
+removeComp l p = if l!!p then (setfalse (let p2 = p^2 in [p2, p2+2*p..n]) l) else l
+  where n = (length l) - 1
 
 
-primes :: Int -> [Int]
-primes 4 = [0, 1, 2, 3]
-primes n = takeWhile (\k->(isPrime n)!!k) [0..n]
+-- primes :: Int -> [Int]
+-- primes 4 = [0, 1, 2, 3]
+-- primes n = filter (!! (isPrime n)) [0..n]
 
 main :: IO ()
-main = do 
-   putStrLn "The primes <= n:"  
-   print ""
+main = do
+   print (let ps = (foldl' removeComp [if (x `mod` 2 == 0) then False else True | x <- [0..10000]] [3, 5..(floor (sqrt 10000))]) in
+     filter (\k -> ps !! k) [0..10000])
 
