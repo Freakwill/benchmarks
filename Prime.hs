bb7d2ef49bb89b1c9f818fe5d1c533325b15570d
William Song <30965609+Freakwill@users.noreply.github.com>
2019-02-21 10:01:32 +0800

    Add files via upload

--- Prime.hs
+++ Prime.hs
@@ -2,35 +2,22 @@ module Prime
 where 
 
 import Data.List
-
-set :: [a] -> Int -> a -> [a]
-set [] n b = []
-set (a:as) 0 b = b:as
-set (a:as) n b = a : (set as (n - 1) b)
+import Data.Array
 
 
-setfalse :: [Int] -> [Bool] -> [Bool]
-setfalse [] a = a
-setfalse (i:is) a = set (setfalse is a) i False
+setFalse :: [Int] -> (Array Int Bool) -> (Array Int Bool)
+setFalse [] a = a
+setFalse s a = let n = (length a)-1 in listArray (0, n) [if k `elem` s then False else a!k | k <- [0..n]]
 
+setfalseComp :: (Array Int Bool) -> Int -> (Array Int Bool)
+setfalseComp l p = if l!p then (setFalse (let p2 = p^2 in [p2, p2+2*p..((length l)-1)]) l) else l
 
--- isPrime :: Int -> [Bool]
--- isPrime 1 = [True, True]
--- isPrime 2 = [False, False, True]
--- isPrime n = let f = [if (x `mod` 2 == 0) then False else True | x <- [0..n]] in
---  foldl removeComp f (slice 3 (floor (sqrt n)) 2) 
+odds :: Int -> (Array Int Bool)
+odds n = listArray (0,n) [x `mod` 2 == 1| x<- [0..n]]
 
-removeComp :: [Bool] -> Int -> [Bool]
-removeComp l p = if l!!p then (setfalse (let p2 = p^2 in [p2, p2+2*p..n]) l) else l
-  where n = (length l) - 1
-
-
--- primes :: Int -> [Int]
--- primes 4 = [0, 1, 2, 3]
--- primes n = filter (!! (isPrime n)) [0..n]
+primes :: (Array Int Bool)
+primes = foldl' setfalseComp (odds 100000) [3, 5..(floor (sqrt 100000))]
 
 main :: IO ()
 main = do
-   print (let ps = (foldl' removeComp [if (x `mod` 2 == 0) then False else True | x <- [0..10000]] [3, 5..(floor (sqrt 10000))]) in
-     filter (\k -> ps !! k) [0..10000])
-
+    print (primes ! 100000)
